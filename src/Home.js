87222d001c101c0dc84640fa3f50484cf6880538
Mbali <mbalikhumalo408@gmail.com>
2023-06-14 21:07:55 +0200

    added

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/style.css";
+
 const Home = () => {
+    const picture = new URL('./img/polygon-scatter-haikei.png', import.meta.url)
     return (
         <div>
             <center>
-            <h1>
-                Welcome
-            </h1>
+            <h1>Welcome :)</h1>
+            <div> 
+                <img src={picture} alt=""/>
+                <center>
+                <Link className="btn btn-success" to={'/register'}>New User</Link>
+                </center>
+            </div>
             </center>
         </div>
     );
--- src/Login.js
+++ src/Login.js
@@ -1,45 +1,122 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
+    const [email, emailupdate] = useState('');
+    const [password, passwordupdate] = useState('');
 
-    const [email, emailupdate] = useState("");
-    const [password, passwordupdate] = useState("");
+    const usenavigate=useNavigate();
+
+    useEffect(()=>{
+sessionStorage.clear();
+    },[]);
 
     const ProceedLogin = (e) => {
         e.preventDefault();
+        if (validate()) {
+            fetch("http://localhost:5000/users/" + email).then((res) => {
+                return res.json();
+            }).then((resp) => {
+
+                if (Object.keys(resp).length === 0) {
+                    toast.error('Please Enter valid email');
+                } else {
+                    if (resp.password === password) {
+                        toast.success('Success');
+                        sessionStorage.setItem('email',email);
+                        sessionStorage.setItem('userrole',resp.role);
+                        usenavigate('/')
+                    }else{
+                        toast.error('Please Enter valid credentials');
+                    }
+                }
+            }).catch((err) => {
+                toast.error('Login Failed due to :' + err.message);
+            });
+        }
+    }
+
+    const ProceedLoginusingAPI = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            ///implentation
+            // console.log('proceed');
+            let inputobj={"email": email,
+            "password": password};
+            fetch("https://localhost:44308/User/Authenticate",{
+                method:'POST',
+                headers:{'content-type':'application/json'},
+                body:JSON.stringify(inputobj)
+            }).then((res) => {
+                return res.json();
+            }).then((resp) => {
+                console.log(resp)
+                if (Object.keys(resp).length === 0) {
+                    toast.error('Login failed, invalid credentials');
+                }else{
+                     toast.success('Success');
+                     sessionStorage.setItem('email',email);
+                     sessionStorage.setItem('jwttoken',resp.jwtToken);
+                   usenavigate('/')
+                }
+                // if (Object.keys(resp).length === 0) {
+                //     toast.error('Please Enter valid email');
+                // } else {
+                //     if (resp.password === password) {
+                //         toast.success('Success');
+                //         sessionStorage.setItem('email',email);
+                //         usenavigate('/')
+                //     }else{
+                //         toast.error('Please Enter valid credentials');
+                //     }
+                // }
+            }).catch((err) => {
+                toast.error('Login Failed due to :' + err.message);
+            });
+        }
+    }
+    const validate = () => {
+        let result = true;
+        if (email === '' || email === null) {
+            result = false;
+            toast.warning('Please Enter email');
+        }
+        if (password === '' || password === null) {
+            result = false;
+            toast.warning('Please Enter Password');
+        }
+        return result;
     }
     return (
-        <div>
-            <div className="row">
-                <div className="offset-lg-3 col-lg-6">
-                    <form onSubmit={ProceedLogin} className="container">
-                        <div className="card">
-                            <div className="card-header">
-                                <h2>Login Details</h2>
+        <div className="row">
+            <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
+                <form onSubmit={ProceedLogin} className="container">
+                    <div className="card">
+                        <div className="card-header">
+                            <h2>User Login</h2>
+                        </div>
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>email<span className="errmsg">*</span></label>
+                                <input value={email} onChange={e => emailupdate(e.target.value)} className="form-control"></input>
                             </div>
-                            <div className="card-body">
-                                <div className="form-group">
-                                    <label> Email <span className="errmsg">*</span></label>
-                                    <input className="form-control"></input>
-                                </div>
-                            </div>
-                            <div className="card-body">
-                                <div className="form-group">
-                                    <label> Password <span className="errmsg">*</span></label>
-                                    <input className="form-control"></input>
-                                </div>
-                            </div>
-                            <div className="card-footer">
-                                <button type="submit" className="btn btn-primary">Login</button> |
-                                <Link className="btn btn-success" to={'/register'}>New User</Link> |
-                                <Link className="btn btn-success" to={'/todolist'}>ToDo</Link>
+                            <div className="form-group">
+                                <label>Password <span className="errmsg">*</span></label>
+                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Login</button> |
+                            <Link className="btn btn-success" to={'/register'}>New User</Link>
+                            <Link className="btn btn-success" to={'/todolist'}>ToDo</Link>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
 }
+
 export default Login;
